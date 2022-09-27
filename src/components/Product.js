002bd47ac0ba03e9a8c284496ca19cd44ecec53e
Vinicius Mussato <vmussato@vinicius-pc.localdomain>
2022-09-27 15:29:23 -0300

    lists

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,13 @@
 export default function Product() {
 
-    const products = ['Laptop', 'Phone', 'Moden', 'Computer'];
-    const productList = products.map((product)=> (<h3>{product}</h3>
+    // const products = ['Laptop', 'Phone', 'Moden', 'Computer', 'Laptop'];
+    const products = [
+        {id: 1, name:"Laptop", price: 500},
+        {id: 2, name:"Phone", price: 700},
+        {id: 3, name:"Wallet", price: 200},
+        {id: 4, name:"Laptop", price: 1000},
+    ]
+    const productList = products.map((product, index)=> (<h3 key={index}>{product.name}: ${product.price}</h3>
     ))
 
     return <div>
